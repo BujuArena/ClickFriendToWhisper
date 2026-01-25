a24cef6a65e93422d291380a219a139d8d559486
Alex Folland <lexlexlex@gmail.com>
2026-01-25 13:22:27 -0800

    support BetterFriendlist and simplify packaging

--- ClickFriendToWhisper.lua
+++ ClickFriendToWhisper.lua
@@ -3,3 +3,14 @@ hooksecurefunc(FriendsListButtonMixin, "OnClick", function(self, button, ...)
 		FriendsFrameSendMessageButton_OnClick()
 	end
 end)
+
+if BetterFriendsList_Button_OnClick then
+	hooksecurefunc("BetterFriendsList_Button_OnClick", function(button,mouseButton, ...)
+		if mouseButton == "LeftButton" and
+		BetterFriendsFrame and
+		BetterFriendsFrame.SendMessageButton and
+		BetterFriendsFrame.SendMessageButton.Click then
+			BetterFriendsFrame.SendMessageButton:Click("LeftButton", true)
+		end
+	end)
+end
