4ef562651b2b053242ae3e2574470116fb61c2fc
Alex Folland <lexlexlex@gmail.com>
2026-02-10 16:04:10 -0800

    support classic versions

--- ClickFriendToWhisper.lua
+++ ClickFriendToWhisper.lua
@@ -1,11 +1,26 @@
-hooksecurefunc(FriendsListButtonMixin, "OnClick", function(self, button, ...)
-	if button == "LeftButton" then
-		FriendsFrameSendMessageButton_OnClick()
-	end
-end)
+-- mainline
+if FriendsListButtonMixin and FriendsListButtonMixin.OnClick then
+	hooksecurefunc(FriendsListButtonMixin, "OnClick", function(self, button, ...)
+		if button == "LeftButton" then
+			FriendsFrameSendMessageButton_OnClick()
+		end
+	end)
+end
 
+-- classic
+if FriendsFrameFriendButton_OnClick then
+	hooksecurefunc("FriendsFrameFriendButton_OnClick", function(button, mouseButton, ...)
+		if mouseButton == "LeftButton" and
+		FriendsFrameSendMessageButton and
+		FriendsFrameSendMessageButton.Click then
+			FriendsFrameSendMessageButton:Click("LeftButton", true)
+		end
+	end)
+end
+
+-- betterfriendslist
 if BetterFriendsList_Button_OnClick then
-	hooksecurefunc("BetterFriendsList_Button_OnClick", function(button,mouseButton, ...)
+	hooksecurefunc("BetterFriendsList_Button_OnClick", function(button, mouseButton, ...)
 		if mouseButton == "LeftButton" and
 		BetterFriendsFrame and
 		BetterFriendsFrame.SendMessageButton and
